7dbcff0931571de8810174c2c6527fe0701ab3bd
porsche <porsche@porsche.local>
2017-03-14 14:36:47 +0800

    添加漫画、添加插画：增加封面

--- src/components/page/column/IllustrationAdd.vue
+++ src/components/page/column/IllustrationAdd.vue
@@ -11,6 +11,12 @@
 
     <div class="form-box">
       <el-form ref="form" :model="comics" label-width="80px">
+        <el-form-item label="封面上传">
+          <el-upload action="" :file-list="comics.fileList" list-type="picture">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">建议尺寸520x676，只能上传jpg/png文件，且不超过1MB</div>
+          </el-upload>
+        </el-form-item>
         <el-form-item label="插画上传">
           <el-upload action="" list-type="picture-card">
             <i class="el-icon-plus"></i>
