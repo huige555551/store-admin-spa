a35a00e62b452f31f6a931c22df6d70121921d0b
huige555551 <1026367919@qq.com>
2017-03-29 17:11:44 +0800

    正在写活动列表

--- src/components/page/activity/Activity.vue
+++ src/components/page/activity/Activity.vue
@@ -154,6 +154,10 @@
 </template>
 
 <script>
+import api from '@/api'
+
+const _ = require('lodash')
+
 export default {
   data() {
     return {
@@ -186,3 +190,118 @@ export default {
   }
 }
 </script>
+
+<script>
+import api from '@/api'
+
+const _ = require('lodash')
+
+export default {
+  data() {
+    return {
+      // 预览
+      articleDialog: false,
+      rowObj: {
+        id: null,
+        title: null,
+        coverUrl: null,
+        navigationName: null,
+        author: null,
+        publicationDate: null
+      },
+      // 搜索
+      searchInput: {
+        title: null,
+        author: null,
+        column: null
+      },
+      searchKey: {
+        title: null,
+        author: null,
+        column: null
+      },
+      // 分页
+      total: 0,
+      currentPage: 1,
+      perPage: 10,
+      // 表格
+      tableData: []
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    // 新窗口打开轮播图
+    openImg(url) {
+      window.open(url)
+    },
+    // 清空搜索
+    emptySearch() {
+      this.searchInput.title = null
+      this.searchInput.author = null
+      this.searchInput.column = null
+      this.searchKey.title = null
+      this.searchKey.author = null
+      this.searchKey.column = null
+      this.currentPage = 1
+      this.fetchData()
+    },
+    // 搜索
+    search() {
+      this.searchKey.title = this.searchInput.title
+      this.searchKey.author = this.searchInput.author
+      this.searchKey.column = this.searchInput.column
+      this.currentPage = 1
+      this.fetchData()
+    },
+    // 获取文章列表数据
+    async fetchData() {
+      this.tableData = []
+      const { code, data } = await api.get('/api/system/wechat/listArticle', {
+        currentPage: this.currentPage,
+        perPage: this.perPage,
+        title: this.searchKey.title,
+        column: this.searchKey.column,
+        author: this.searchKey.author
+      })
+      if (code === 200) {
+        this.tableData = data.array
+        this.total = data.total
+      }
+    },
+    // 删除行
+    deleteRow(index) {
+      this.$confirm('此操作将该删除该文章，是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'info'
+      }).then(async () => {
+        const { code } = await api.post('/api/system/wechat/deleteArticle', { articleId: this.tableData[index].id })
+        if (code === 200) {
+          this.tableData.splice(index, 1)
+          this.$notify.success({ title: '成功', message: '删除成功' })
+          this.fetchData()
+        }
+      }).catch(() => {})
+    },
+    // 查看
+    checkDetail(index) {
+      this.rowObj = _.clone(this.tableData[index])
+      this.articleDialog = true
+    },
+    // 分页
+    handleSizeChange(val) {
+      this.perPage = val
+      this.currentPage = 1
+      this.fetchData()
+    },
+    // 分页
+    handleCurrentChange(val) {
+      this.currentPage = val
+      this.fetchData()
+    }
+  }
+}
+</script>
+
