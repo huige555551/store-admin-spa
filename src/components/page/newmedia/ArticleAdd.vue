0994bcac18873d84f5f80a1e48fc85dde8b82725
u14e <u14e@u14edeMacBook-Pro.local>
2017-05-08 17:07:18 +0800

    fixed

--- src/components/page/newmedia/ArticleAdd.vue
+++ src/components/page/newmedia/ArticleAdd.vue
@@ -70,7 +70,7 @@
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="save">提交</el-button>
-          <el-button>取消</el-button>
+          <el-button @click="$router.push('/newmedia/list')">取消</el-button>
         </el-form-item>
       </el-form>
     </div>
